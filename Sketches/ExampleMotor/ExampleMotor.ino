426eab4a962ee7663268ac5f8780638c466b86a0
Suhail Hasan Kg <official.aitra.tech@gmail.com>
2023-07-12 18:04:04 +0530

    made changes for ExampleMotor.ino

--- Sketches/ExampleMotor/ExampleMotor.ino
+++ Sketches/ExampleMotor/ExampleMotor.ino
@@ -1,14 +1,14 @@
-
+int MotorPin = 7;
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(8, OUTPUT);
+  // initialize digital pin 8 as an output.
+  pinMode(MotorPin, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(8, HIGH);  // turn the MOTOR on (HIGH is the voltage level)
-  delay(10000);                      // wait for 10 second
-  digitalWrite(8, LOW);   // turn the MOTOR off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  digitalWrite(MotorPin, HIGH);  // turn the MOTOR on (HIGH is the voltage level)
+  // delay(10000);                      // wait for 10 second
+  // digitalWrite(8, LOW);   // turn the MOTOR off by making the voltage LOW
+  // delay(1000);                      // wait for a second
 }
